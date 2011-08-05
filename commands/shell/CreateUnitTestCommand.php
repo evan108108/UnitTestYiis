c4f8f086590154d709f078717b464039cecbb22d
evan108108 <evan108108@gmail.com>
2011-08-04 23:56:56 -0300

    Fixed Typos and small bugs

--- commands/shell/CreateUnitTestCommand.php
+++ commands/shell/CreateUnitTestCommand.php
@@ -36,36 +36,43 @@ EOD;
         echo "fixture name can not be NULL";
     }
     
-    echo "Please enter the fixture key prefix used in your fixture file. \nExample `user_` as in ('user_1'=>array('id'=>1, etc..), user_2=>array('id'=>2, ...), ...).\nThe default is `ModelName_`: ";
+    echo "Please enter the fixture key prefix used in your fixture file. \nExample `user_` as in ('user_1'=>array(...), user_2=>array(...), ...).\nThe default is `ModelName_`: ";
     $fixtureKeyPrefix = trim(fgets(STDIN));
     if($fixtureKeyPrefix == "")
       $fixtureKeyPrefix = $modelName . '_';
+
+    echo "Are there attributes of '$modelName' that are contained in fixture '$fixtureName' modified by beforeSave or other action(s)? [Yes|No]: ";
+    if(!strncasecmp(trim(fgets(STDIN)),'n',1))
+      $check_data_consistency_after_save = 'true';
+    else
+      $check_data_consistency_after_save = 'false';
         
     $unitCodePath = Yii::getPathOfAlias('application.tests.unit');
     $unitCodeFileName = $modelName . 'UnitTest.php';
     if(!file_exists("$unitCodePath/$unitCodeFileName"))
     {
-      file_put_contents("$unitCodePath/$unitCodeFileName", $this->renderUnitCode($modelName, $fixtureName, $fixtureKeyPrefix));
+      file_put_contents("$unitCodePath/$unitCodeFileName", $this->renderUnitCode($modelName, $fixtureName, $fixtureKeyPrefix, $check_data_consistency_after_save));
       echo "Unit test created @ $unitCodePath/$unitCodeFileName";
     }
     else
       echo "A unit test for this model already exists, no test created.";
   }
 
-  private function renderUnitCode($modelName, $fixtureName, $fixtureKeyPrefix)
+  private function renderUnitCode($modelName, $fixtureName, $fixtureKeyPrefix, $check_data_consistency_after_save)
   {
     return '<?php
       /**
        * Created by Evan Frohlich evan.frohlich@controlgroup.com
        */ 
-      Class ' . ucfirst($modelName) . 'UnitTest extends CrudBase
+      Class  ' . ucfirst($modelName) . 'UnitTest extends CrudBase
       { 
         public $fixtures = array(' . "'" . $fixtureName . "'" . '=>' . "'" . $modelName . "'" .  ');
 
         public $modelName = '  . "'" .  $modelName  . "'" . ';         //Reffers to your Model name
         public $fixtureRef = '  . "'" . $fixtureName  . "'" . ';       //Refers to the name of your fixture file
         public $fixtureKeyPrefix = ' . "'" . $fixtureKeyPrefix . "'" . '; //How your fixture items are keyed Sample_1, Sample_2, Etc...
-    
+        public $check_data_consistency_after_save = ' . $check_data_consistency_after_save . ';
+
         public function testGetRecord()
         {
           parent::testGetRecord();
